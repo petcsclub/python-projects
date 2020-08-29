669cca27be14337b142c24cd4d9bfcc3976c71a4
Albert <albertlai631@outlook.com>
2020-08-29 11:14:19 -0400

    finish logic for player ship input

--- battleship/battleship-single.py
+++ battleship/battleship-single.py
@@ -1,64 +1,114 @@
 # Variables
 shipNames = ['Carrier', 'Battleship', 'Cruiser', 'Submarine', 'Destroyer']
 shipLength = [5, 4, 3, 3, 2]
+# player view of player's grid
 playerGrid = [[' ' for i in range(10)] for j in range(10)]
+# player view of computer's grid
 computerGrid = [[' ' for i in range(10)] for j in range(10)]
-playerShipCoords = {}
-computerShipCoords = {}
+playerShipCoords = [set() for i in range(5)]
+computerShipCoords = [set() for i in range(5)]
 
 
 def setPlayerShips():
     '''Get the ship placements of the player'''
     for i in range(len(shipNames)):
         while True:
-            print('Set the position of your' +
-                  shipNames[i] + ' of length ' + shipLength[i] + '.')
+            print('Set the position of your ' +
+                  shipNames[i] + ' of length ' + str(shipLength[i]) + '.')
 
             # Get start coordinate and check validity
-            first = input('Coordinate for the start of your ' +
-                          shipNames[i] + '?')
-            firstCoords = convert(first)
-            if firstCoords is None:
+            start = input('Coordinate for the start of your ' +
+                          shipNames[i] + '? ')
+            startCoords = convert(start)
+            if startCoords is None:
                 print('Invalid input. Please try again.')
                 continue
 
             # Get end coordinate and check validity
-            second = input('Coordinate for the end of your ' +
-                           shipNames[i] + '?')
-            secondCoords = convert(second)
-            if secondCoords is None:
+            end = input('Coordinate for the end of your ' +
+                        shipNames[i] + '? ')
+            endCoords = convert(end)
+            if endCoords is None:
                 print('Invalid input. Please try again.')
                 continue
 
             # Check valid ship length and row/column
-            if firstCoords[0] == secondCoords[0]:
-                if abs(firstCoords[1]-secondCoords[1]) + 1 != shipLength[i]:
+            if startCoords[0] == endCoords[0]:
+                if abs(startCoords[1]-endCoords[1]) + 1 != shipLength[i]:
                     print(
-                        'Invalid ship length. Please try again')
+                        'Invalid ship length. Please try again.')
                     continue
-            elif firstCoords[1] == secondCoords[1]:
-                if abs(firstCoords[0]-secondCoords[0]) + 1 != shipLength[i]:
+            elif startCoords[1] == endCoords[1]:
+                if abs(startCoords[0]-endCoords[0]) + 1 != shipLength[i]:
                     print(
-                        'Invalid ship length. Please try again')
+                        'Invalid ship length. Please try again.')
                     continue
             else:
                 print(
-                    'The start and end of the ship must be on the same row or column. Please try again')
+                    'The start and end of the ship must be on the same row or column. Please try again.')
                 continue
+
+            # Check validity of ship placement
+            if checkShipPlacement(startCoords, endCoords, i, playerShipCoords, playerGrid):
+                break
+            else:
+                print('Ships cannot intersect. Please try again.')
+                continue
+
+
+def checkShipPlacement(startCoords, endCoords, shipIndex, shipCoords, grid=None):
+    # Check validity
+    if startCoords[0] == endCoords[0]:
+        # first coordinate is the same
+        if startCoords[1] > endCoords[1]:
+            startCoords, endCoords = endCoords, startCoords
+        # check validity
+        for i in range(startCoords[1], endCoords[1]+1):
+            if hitShip((startCoords[0], i), shipCoords, shipIndex) >= 0:
+                return False
+        # input ships
+        for i in range(startCoords[1], endCoords[1]+1):
+            shipCoords[shipIndex].add((startCoords[0], i))
+            if grid != None:
+                grid[startCoords[0]][i] = 's'
+        return True
+    else:
+        # second coordinate is the same
+        if startCoords[0] > endCoords[0]:
+            startCoords, endCoords = endCoords, startCoords
+        # check validity
+        for i in range(startCoords[0], endCoords[0]+1):
+            if hitShip((i, startCoords[1]), shipCoords, shipIndex) >= 0:
+                return False
+        # input ships
+        for i in range(startCoords[0], endCoords[0]+1):
+            shipCoords[shipIndex].add((i, startCoords[1]))
+            if grid != None:
+                grid[i][startCoords[1]] = 's'
+        return True
 
 
 def convert(coords):
     '''Convert coords in the correct form to the 2D array coordinates'''
-    first = ord(coords[0].upper())
-    second = coords[1]
-    firstValid = first >= 65 and first <= 74
-    secondValid = second >= 48 and second <= 57
-    if coords.length == 2 and firstValid and secondValid:
-        return(first-65, second-48)
+    if len(coords) == 2:
+        first = ord(coords[0].upper())
+        second = ord(coords[1])
+        firstValid = first >= 65 and first <= 74
+        secondValid = second >= 48 and second <= 57
+        if firstValid and secondValid:
+            return(first-65, second-48)
+
+
+def hitShip(coords, shipCoords, shipIndex=5):
+    '''Check if coordinate intersects with existing ship, and return the index of the ship if it does. If not, return -1'''
+    for i in range(shipIndex):
+        if coords in shipCoords[i]:
+            return i
+    return -1
 
 
 # Game loop
 while True:
     print('Welcome to Battleship!')
+    setPlayerShips()
     break
-    # setShips()
